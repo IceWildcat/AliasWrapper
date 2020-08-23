762e7a577022b0cc168071ec0ea16bda18f5d013
Marcos <margual56@gmail.com>
2020-08-24 00:21:36 +0200

    Added the "ls" command
    * Renamed ambiguous variables
    * Functionality for the ls command with  the "-a" argument
    * Added TODO with the remaining arguments

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ class wShell(cmd.Cmd):
     def do_alias(self, args: str):  # TODO: clean this shit
         """Register or check a command alias. It autosaves it to the alias file.
       Usage: alias <alias> [command] [args...]"""
-        l = args.split(' ')
+        args_list = args.split(' ')
         # print(l)
         if args == '':
             self.stdout.write(self.do_alias.__doc__)
             self.stdout.write(f"\nAliases: {str(self.aliases)}\n")
         else:
             cmd, arg, lin = self.parseline(args)
-            if self.aliases.get(l[0], None):
-                if len(l) < 2:
-                    self.stdout.write(f'{l[0]}: {str(self.aliases.get(l[0], "You fucked up"))}')
+            if self.aliases.get(args_list[0], None):
+                if len(args_list) < 2:
+                    self.stdout.write(f'{args_list[0]}: {str(self.aliases.get(args_list[0], "You fucked up"))}')
                 else:
                     self.stdout.write(
-                        f'MODIFYING {l[0]}: {str(self.aliases.get(l[0], "?¿?¿Data race?"))} -> {str(arg)}')
-                    self.aliases[l[0]] = arg
+                        f'MODIFYING {args_list[0]}: {str(self.aliases.get(args_list[0], "?¿?¿Data race?"))} -> {str(arg)}')
+                    self.aliases[args_list[0]] = arg
             else:
                 print(arg)
-                self.aliases[l[0]] = arg
+                self.aliases[args_list[0]] = arg
             with open(self.aliasfile, 'w') as a:  # TODO (1): handle exceptions
                 a.write(json.dumps(self.aliases))
                 self.stdout.write("Aliases saved.\n")
@@ -41,19 +41,46 @@ class wShell(cmd.Cmd):
 
         return
 
+    def do_ls(self, args: str):  # TODO: flags dFil[a,h,s]rRsStX
+        ls_dir = '.'
+        if len(args) > 1:
+            args_index = 0 if args[0].startswith('-') else 1  # Find the index for the flags
+
+            args_list = [arg for arg in args[args_index] if not arg == '-']  # With that index, get the flags as a list
+
+            ls_dir = args[1 - args_index]  # The directory is the other argument
+
+            if os.path.isdir(ls_dir):  # If the directory does not exist, return error
+                return 1
+
+        else:
+            args_list = [arg for arg in args
+                         if not arg == '-']  # If there is only one argument (or none) it is much more straight-forward
+
+        # Get a sorted list of all the files inside the directory.
+        files = sorted([f for f in os.listdir(ls_dir)
+                        if ('a' in args_list or not f.startswith('.'))  # If not flag 'a', show only not hidden files
+                        ], key=lambda f: f.lower())
+        # print(*files, sep='\t\t')  # TODO: Wrap the lines
+
+        files_output = "\t\t".join(files)
+        self.stdout.write(f'{files_output}\n')
+
+        return 0  # Success
+
     def do_unalias(self, args: str):
         """Unregister a command alias. It autosaves it to the alias file.
       Usage: unalias <alias>"""
-        l = args.split(' ')
+        args_list = args.split(' ')
         # print(l)
         if args == '':
             self.stdout.write(self.do_alias.__doc__)
         else:
             cmd, arg, lin = self.parseline(args)
-            if self.aliases.pop(l[0], None):
-                self.stdout.write(f'"{l[0]}" removed.\n')
+            if self.aliases.pop(args_list[0], None):
+                self.stdout.write(f'"{args_list[0]}" removed.\n')
             else:
-                self.stdout.write(f'{l[0]} was not an alias.\n')
+                self.stdout.write(f'{args_list[0]} was not an alias.\n')
             with open(self.aliasfile, 'w') as a:  # TODO (1): handle exceptions
                 a.write(json.dumps(self.aliases))
                 self.stdout.write("Aliases saved.\n")
@@ -70,7 +97,8 @@ class wShell(cmd.Cmd):
         """Changes the current working directory."""
         n = -1
         try:
-            n = os.chdir(args)
+            os.chdir(args)
+            n = 0
         except OSError as e:
             n = e.errno
             self.stdout.write(f"cd: {e.strerror}\n\n")
@@ -88,7 +116,7 @@ class wShell(cmd.Cmd):
     def do_shell(self, line: str):
         return os.system(line)
 
-    def command_in_path(self, exe):
+    def command_in_path(self, exe: str):
         """Checks in every address in the system PATH enviroment variable whether the command exists or not. If it does,
         it executes it. Otherwise, returns False"""
 
@@ -96,8 +124,9 @@ class wShell(cmd.Cmd):
             if not os.path.isdir(path):  # If the PATH address is not valid, move on
                 continue
 
-            if exe + '.exe' in os.listdir(path):  # However, if an executable file with the given name exists,
-                return True   # Execute it with the given arguments
+            if exe + '.exe' in os.listdir(
+                    path):  # However, if an executable file with the given name exists, return True
+                return True
 
         return False  # Otherwise, return False (the command is not in PATH)
 
@@ -106,11 +135,11 @@ class wShell(cmd.Cmd):
         return
 
     def default(self, line: str):
-        l = line.split(' ')
-        if self.aliases.get(l[0], None):
-            self.onecmd(self.aliases[l[0]])
-        elif self.command_in_path(l[0]):
-            return os.system(' '.join(l))
+        args_list = line.split(' ')
+        if self.aliases.get(args_list[0], None):
+            self.onecmd(self.aliases[args_list[0]])
+        elif self.command_in_path(args_list[0]):
+            return os.system(' '.join(args_list))
         else:
             super().default(line)
 

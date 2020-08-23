4c8ecabd99772758cc1f499896f5e0af4bdc5273
Marcos <margual56@gmail.com>
2020-08-23 19:59:35 +0200

    Fixed minor syntax errors, added a .gitignore and reformatted titles of README.
    * About the syntax errors, they were python-specific. Meaning that they were things like using parenthesis in the if statements. Nothing serious.
    
    * I need the .gitignore because I am using Pycharm, which generates a .idea folder.
    
    * The README titles were not formatting correctly because of adding "---" under the titles (the titles already have a separator)

--- README.md
+++ README.md
@@ -2,26 +2,23 @@
 
 Yeah.
 Probably not very useful on GNU/Linux envs, or even any environment different than Windows.
-#### This thing allows you to have autosaving command aliases. Period.
+##### This thing allows you to have autosaving command aliases. Period.
 
 
 ## IMPORTANT NOTES
----
 * Implementation is done using... Bad practices. This is not a replacement for your shell (yet). Please use with caution.
 * DO NOT BLAME ME IF YOU USE THIS IN A CHAIN OR SOMETHING IMPORTANT AND IT BREAKS THINGS.
 * You can use shell commands and execute things by putting `!` before anything. THIS WRAPPER DOES NOT CHECK YOUR PATH YET.
 
 
-### Things to do
----
+## Things to do
 * Check for commands in PATH.
 * Cleanup.
 * Testing.
 * Change internal system to have a shell subprocess instead of shouting system calls.
 
 
-### How to use
----
+## How to use
 1. Open with your favourite python(3) version.
 2. Type `help`
 3. Add aliases
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os,cmd,json
+import cmd
+import json
+import os
+
 
 class wShell(cmd.Cmd):
     prompt = str(os.getcwd()) + '>'
@@ -6,65 +9,66 @@ class wShell(cmd.Cmd):
         'exit': 'quit'
     }
     aliasfile = str(os.path.expanduser("~")) + "\\.alias.cfg"
-    
+
     def emptyline(self):
         return
-    
-    def do_alias(self, args: str): #TODO: clean this shit
+
+    def do_alias(self, args: str):  # TODO: clean this shit
         """Register or check a command alias. It autosaves it to the alias file.
       Usage: alias <alias> [command] [args...]"""
         l = args.split(' ')
-        #print(l)
-        if(args == ''):
+        # print(l)
+        if args == '':
             self.stdout.write(self.do_alias.__doc__)
             self.stdout.write(f"\nAliases: {str(self.aliases)}\n")
         else:
             cmd, arg, lin = self.parseline(args)
-            if(self.aliases.get(l[0], None)):
-                if(len(l < 2)):
+            if self.aliases.get(l[0], None):
+                if len(l) < 2:
                     self.stdout.write(f'{l[0]}: {str(self.aliases.get(l[0], "You fucked up"))}')
                 else:
-                    self.stdout.write(f'MODIFYING {l[0]}: {str(self.aliases.get(l[0], "?¿?¿Data race?"))} -> {str(arg)}')
+                    self.stdout.write(
+                        f'MODIFYING {l[0]}: {str(self.aliases.get(l[0], "?¿?¿Data race?"))} -> {str(arg)}')
                     self.aliases[l[0]] = arg
             else:
                 print(arg)
                 self.aliases[l[0]] = arg
-            with open(self.aliasfile, 'w') as a: #TODO (1): handle exceptions 
+            with open(self.aliasfile, 'w') as a:  # TODO (1): handle exceptions
                 a.write(json.dumps(self.aliases))
                 self.stdout.write("Aliases saved.\n")
-        
+
         self.stdout.write("\n")
-        
+
         return
 
     def do_unalias(self, args: str):
         """Unregister a command alias. It autosaves it to the alias file.
       Usage: unalias <alias>"""
         l = args.split(' ')
-        #print(l)
-        if(args == ''):
+        # print(l)
+        if args == '':
             self.stdout.write(self.do_alias.__doc__)
         else:
             cmd, arg, lin = self.parseline(args)
-            if(self.aliases.pop(l[0], None)):
+            if self.aliases.pop(l[0], None):
                 self.stdout.write(f'"{l[0]}" removed.\n')
             else:
                 self.stdout.write(f'{l[0]} was not an alias.\n')
-            with open(self.aliasfile, 'w') as a: #TODO (1): handle exceptions 
+            with open(self.aliasfile, 'w') as a:  # TODO (1): handle exceptions
                 a.write(json.dumps(self.aliases))
                 self.stdout.write("Aliases saved.\n")
-        
+
         self.stdout.write("\n")
-        
+
         return
 
-    def do_quit(self, args:str):
+    def do_quit(self, args: str):
         """Exit the program."""
         exit(0)
-    
-    def do_cd(self, args:str):
+
+    def do_cd(self, args: str):
         """Changes the current working directory."""
-        n = -1;
+        n = -1
         try:
             n = os.chdir(args)
         except OSError as e:
@@ -72,41 +76,38 @@ class wShell(cmd.Cmd):
             self.stdout.write(f"cd: {e.strerror}\n\n")
         finally:
             return n
-        
 
-    def do_reloadalias(self, line:str): #TODO (1): handle exceptions
+    def do_reloadalias(self, line: str):  # TODO (1): handle exceptions
         """Reloads the alias file and parses it into internal memory."""
         with open(self.aliasfile, 'r') as a:
             self.aliases = json.loads(a.read())
             self.stdout.write("Aliases reloaded. Running `alias`...\n")
             self.onecmd("alias")
         return 0
-    
-    def do_shell(self, line:str):
+
+    def do_shell(self, line: str):
         return os.system(line)
-    
-    def postcmd(self, stop, line:str):
-        self.prompt = str(0 if stop==None else stop) + "<" + str(os.getcwd()) + ">"
+
+    def postcmd(self, stop, line: str):
+        self.prompt = str(0 if stop is None else stop) + "<" + str(os.getcwd()) + ">"
         return
 
-    def default(self, line:str):
+    def default(self, line: str):
         l = line.split(' ')
-        if(self.aliases.get(l[0], None)):
+        if self.aliases.get(l[0], None):
             self.onecmd(self.aliases[l[0]])
         else:
             super().default(line)
-        
+
         return
 
     def __init__(self, completekey='tab', stdin=None, stdout=None):
-        super().__init__(completekey=completekey,stdin=stdin,stdout=stdout)
+        super().__init__(completekey=completekey, stdin=stdin, stdout=stdout)
         if not (os.path.isfile(self.aliasfile)):
             self.stdout.write("Alias file not found, creating...\n")
             with open(self.aliasfile, 'a') as a:
                 a.write(json.dumps(self.aliases))
         self.do_reloadalias("")
-
-
 
 
 if __name__ == "__main__":

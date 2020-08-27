839f25f5593593ae5b71e005f3e881b3864eaa87
IceWildcat <5923109+IceWildcat@users.noreply.github.com>
2020-08-27 15:06:58 +0200

    Fix some docstrings

--- extras/path.py
+++ extras/path.py
@@ -3,9 +3,10 @@ from main import wShell as sh
 
 
 def command_in_path(exe: str):
-    """Checks in every address in the system PATH enviroment variable whether the command exists or not. If it does,
-    it executes it. Otherwise, returns False"""
-
+    """Checks in every address in the system PATH enviroment variable whether the command exists or not. 
+    :param exe: Program name without extension
+    :return: True (exists) | False (not exists)"""
+    #TODO: make platform-independant?
     for path in os.environ['PATH'].split(";"):  # For each value in the PATH
         if not os.path.isdir(path):  # If the PATH address is not valid, move on
             continue
